96bbf92c9a27526cf500af9c9e6aab989fd6ab21
Glenn Goodrich <glenn.goodrich@gmail.com>
2010-09-09 14:22:38 -0400

    User model with passwords and user show page

---
+++ spec/factories.rb
@@ -0,0 +1,7 @@
+# By using the symbol ':user', we get Factory Girl to simulte the User model.
+Factory.define :user do |user|
+  user.name                   "Micheal Hartl"
+  user.email                  "mhart1@example.com"
+  user.password               "foobar"
+  user.password_confirmation  "foobar"
+end
