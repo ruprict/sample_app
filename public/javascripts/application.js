beff12cd6805ca344cb0dfc739227ec5239e1992
Glenn Goodrich <glenn.goodrich@gmail.com>
2010-10-17 11:46:12 -0400

    add jquery and char counter

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,16 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+$(document).ready(function(){
+   
+   $("#micropost_content").keypress(function(){
+        var n = $(this).val().length;
+        if (n>140)
+            $("#charCount").addClass("tooManyChars");
+        else 
+            $("#charCount").removeClass("tooManyChars");
+            
+        $("#charCount").html(140-n); 
+   });
+    
+});
